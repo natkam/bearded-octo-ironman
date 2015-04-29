af418ad952adcd0eab973b5b183d9692b683be4e
Noaal <noaal6@gmail.com>
2015-04-29 08:29:07 +0200

    Refactored rev function to solve list(len=3) reverse

--- Spotkanie4/krzysiek_h4_tests.py
+++ Spotkanie4/krzysiek_h4_tests.py
@@ -4,8 +4,12 @@ from krzysiek_brackets import valid_brackets
 
 class TestHomeworkFour(unittest.TestCase):
         def test_list_reversion(self):
+            list_len_two = [1, 2]
+            list_len_three = [1, 2, 3]
             my_list = [1 ,2 ,3 ,4]
             another = [i for i in range(1 , 101)]
+            self.assertEqual(rev(list_len_two), [2, 1])
+            self.assertEqual(rev(list_len_three), [3, 2, 1])
             self.assertEqual(rev(my_list), [4, 3, 2, 1])
             self.assertEqual(rev(another), [i for i in range(100, 0, -1)])
 
--- Spotkanie4/krzysiek_rev.py
+++ Spotkanie4/krzysiek_rev.py
@@ -1,9 +1,12 @@
 def rev(my_list):
     if my_list:
-        my_list[-1], my_list[0] = my_list[0], my_list[-1]
-        temp = rev(my_list[1:-1])
-        temp.insert(0, my_list[0])
-        temp.append(my_list[-1])
-        return temp
+        if len(my_list) > 1:
+            my_list[-1], my_list[0] = my_list[0], my_list[-1]
+            temp = rev(my_list[1:-1])
+            temp.insert(0, my_list[0])
+            temp.append(my_list[-1])
+            return temp
+        else:
+            return my_list
     else:
         return my_list
